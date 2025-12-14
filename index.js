a5d4857ba725a5850f9deb27fc74cde92f488e06
Muazhere5 <muazctg07@gmail.com>
2025-12-14 21:36:03 +0600

    mongoconnection

--- index.js
+++ index.js
@@ -14,3 +14,33 @@ const port = process.env.PORT || 5000;
 ====================== */
 app.use(cors());
 app.use(express.json());
+
+MONGODB CONNECTION
+====================== */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project00.3ikpony.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let usersCollection;
+let servicesCollection;
+let bookingsCollection;
+let paymentsCollection;
+
+async function connectDB() {
+  await client.connect();
+  const db = client.db("styleDecorDB");
+
+  usersCollection = db.collection("users");
+  servicesCollection = db.collection("services");
+  bookingsCollection = db.collection("bookings");
+  paymentsCollection = db.collection("payments");
+
+  console.log("✅ MongoDB Connected");
+}
+connectDB();
